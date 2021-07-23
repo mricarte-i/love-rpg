e641039dff2df800e55f6d0e944f4b7ad79a8456
mricarte <mricarte@itba.edu.ar>
2021-07-22 21:29:14 -0300

    added center(), and contains() to body comp

--- comps/body.lua
+++ comps/body.lua
@@ -5,6 +5,22 @@ local function createBody(x, y, width, height)
         y = y,
         width = width,
         height = height,
+
+        center = function(self)
+            return {
+                x + width * 0.5,
+                y + height * 0.5,
+            }
+        end,
+
+        contains = function(self, other)
+            assert(type(other) == "table")
+            assert(other.name_ == "body")
+            return self.x + self.width > other.x and
+                    self.x < other.x + other.width and
+                    self.y + self.height > other.y and
+                    self.y < other.y + other.height
+        end,
     }
 end
 
