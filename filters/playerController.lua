1167ef13db6047d9346dedddb8c8e915c3be1098
mricarte <mricarte@itba.edu.ar>
2021-07-23 22:28:19 -0300

    playerController now checks for "player" filter

--- filters/playerController.lua
+++ filters/playerController.lua
@@ -1,5 +1,6 @@
-local PlayerController = Manager:filter {"body", "physics", "statemachine"}
+local PlayerController = Manager:filter {"body", "physics", "statemachine", "player"}
 local createBody = require "comps/body"
+require "utils"
 
 
 function PlayerController:update(entity, dt)
@@ -7,21 +8,37 @@ function PlayerController:update(entity, dt)
     local body = entity:get("body")
     local state = entity:get("statemachine")
 
+    --on the first frame, state has a pointer, second frame its nil, third has a pointer....
+    --local flag = true
+    --if state == nil then
+        --flag = false
+    --end
+
+    --print("entity:", entity.id)
+
     if love.keyboard.isDown("a") then
         physics.vx = physics.vx - physics.speedx * dt
-        state:setState("walk")
+        --if flag then
+            state:setState("walk")
+        --end
     end
     if love.keyboard.isDown("d") then
         physics.vx = physics.vx + physics.speedx * dt
-        state:setState("walk")
+        --if flag then
+            state:setState("walk")
+        --end
     end
     if love.keyboard.isDown("w") then
         physics.vy = physics.vy - physics.speedy * dt
-        state:setState("idle")
+        --if flag then
+            state:setState("idle")
+        --end
     end
     if love.keyboard.isDown("s") then
         physics.vy = physics.vy + physics.speedy * dt
-        state:setState("idle")
+        --if flag then
+            state:setState("idle")
+        --end
     end
 
 end
